e8750cc42f281fbbb10d0a8d9f9c53cbc6d877fe
Joseph Manning <jgmanning3@yahoo.com>
2025-01-10 12:38:46 -0700

    Init commit of Angular client side

---
+++ src/toppings/toppings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toppings',
+  standalone: true,
+  imports: [],
+  templateUrl: './toppings.component.html',
+  styleUrl: './toppings.component.css'
+})
+export class ToppingsComponent {
+
+}
